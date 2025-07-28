a78afde17d9a0c2a533a2e7dd0c08b92dae25550
VaDiamonddd <vadim.davydov77@gmail.com>
2025-07-28 18:27:17 +0200

    calc

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public abstract class Main{
     public static void main(String[] args){
-
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter two numbers and operation you want to use.");
+        double a = input.nextDouble();
+        double b = input.nextDouble();
+        String operator =  input.next();
+        input.close();
+        switch (operator){
+            case "/" -> System.out.println(a / b);
+            case "*" -> System.out.println(a * b);
+            case "+" -> System.out.println(a + b);
+            case "-" -> System.out.println(a - b);
+        }
     }
 }
