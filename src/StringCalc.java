a5d1f33fa4e5873b0aa0bed1fb609f4516c0adc3
VaDiamonddd <vadim.davydov77@gmail.com>
2025-07-30 04:07:53 +0200

    calc

--- src/StringCalc.java
+++ src/StringCalc.java
@@ -1,5 +1,22 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 public class StringCalc {
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter the total operation you want to use. " +
+                "Please adhere to common rules of mathematics. " +
+                "Surround negative numbers with parentheses");
+        String operation = input.nextLine();
+        input.close();
+        int openingOperBr = 0;
+        ArrayList<Integer> brIndices = new ArrayList<>();
+        char[] convertedArr = operation.toCharArray();
+        for (int i = 0; i < convertedArr.length; i++){
+            if (convertedArr[i] == '(' && i != (convertedArr.length - 1) && convertedArr[i+1] != '-'){
+                openingOperBr += 1;
+                brIndices.add(i);
+            }
+        }
+        
     }
 }
